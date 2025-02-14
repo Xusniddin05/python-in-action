6511aa3d36d7eec58caf9c52156ccff3654a05fe
Xusniddin05 <161422442+Xusniddin05@users.noreply.github.com>
2025-02-14 20:25:01 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,2 +1,16 @@
 def typeBasedTransformer(**kwargs):
-  
+  changed_kwargs= {}
+  for key, value in kwargs.items():
+    if isinstance(value, (int, float)):
+      changed_kwargs[key] = value ** 2
+    elif isinstance(value, str):
+      changed_kwargs[key] = value[::-1]
+    elif isinstance(value, bool):
+      changed_kwargs[key] = not value
+    elif isinstance(value, (list, tuple):
+      changed_kwargs[key] = value[::-1]
+    elif isinstance(value, dict):
+      changed_kwargs[key] = {v: k for k, v in value.items()}
+    else:
+      changed_kwargs[key] = value
+return changed_kwargs   
